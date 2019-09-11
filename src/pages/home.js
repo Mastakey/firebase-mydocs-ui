83a04982834ff0a17f5e929b47db5ea9d30a37cd
Han <Kioh.Han@espn.com>
2019-09-11 17:21:41 -0400

    axios

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 export class home extends Component {
+    state = {
+        screams: null
+    }
+    async componentDidMount(){
+        try {
+            let mydocs = await axios.get(`/mydoc`);
+            console.log(mydocs.data);
+            this.setState({
+                screams: mydocs.data
+            });
+        }
+        catch (err) {
+            console.log(err);
+        }
+
+
+    }
     render() {
         return (
             <div>
